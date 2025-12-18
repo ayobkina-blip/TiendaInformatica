4b7a6729ba9181ebe485a9c0e415b1f16cf804d0
ayobkina-blip <ayobkina@gmail.com>
2025-12-18 20:57:12 +0100

    Version con servicio usuario

---
+++ app/Providers/UserProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UUserProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function registrarUsuario(){
+        
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
