572e685c9818d9de55805ca57a1a636686a25a8b
ayobkina-blip <ayobkina@gmail.com>
2026-01-08 21:00:14 +0100

    Tercer commit: Menu empezado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,19 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('titulo')</title>   
+    <title>@yield('titulo')</title>
+    <link rel="stylesheet" href="{{asset('css/menu.css')}}">   
 </head>
 
 <body>
 
     <!-- Encabezado -->
 
-    <header>
-        <div class="encabezado">
-            <div class="logo">
-                PcDispositivos
-            </div>
+    <header class="main-header">
+    
+        <img src="{{ asset('images/logo.png') }}" class="logo-img" alt="">
+
+        <div class="contenedor-principal">
+            <form class="buscador-caja">
+                <input type="text" class="buscador-input" placeholder="Buscar artículos, marcas...">
+                <button type="submit" class="buscador-boton">Buscar</button>
+            </form>
         </div>
+
     </header>
     
     @yield('contenido')  
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -4,6 +4,5 @@
 
 @section('contenido')
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi alias, atque assumenda officia velit tempore incidunt voluptatibus rerum. Itaque at quo molestiae aspernatur deserunt quas, accusantium nesciunt numquam nostrum voluptatibus?</p>
 
 @endsection
